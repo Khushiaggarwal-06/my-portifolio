fbf7adf892c08e88bd34c5d8c5e6742180f70e20
khushiaggarwal-06 <khushi0383.be20@chitkara.edu.in>
2022-08-03 19:49:57 +0530

    First commit

---
+++ script1.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ var typed = new typed(".typing", {
+        strings: ["Welcome to my personal website",""],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    
+});
